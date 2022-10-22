168022f85e716463eb7ea109c23a998816900f0a
dazzlemon <daanill13470@gmail.com>
2022-10-22 20:52:31 +0300

    doc

--- 1/machine_epsilon.hs
+++ 1/machine_epsilon.hs
@@ -7,9 +7,11 @@ machineEpsilon = last $ takeWhile (\x -> 1 + x /= 1)
 
 machineEpsilon' :: Double
 machineEpsilon' = 2 * until ((1 ==) . (1 +)) (/ 2) 1
+-- 2 * is optional to be of the same value as machineEpsilon
 
 machineEpsilon'' :: Double
 machineEpsilon'' = 2 * until (liftM2 (.) (==) (+) 1) (/ 2) 1
+-- 2 * is optional to be of the same value as machineEpsilon
 
 main :: IO ()
 main = mapM_ print [ machineEpsilon
